e764cdbc69d4af249225a1fa277d55ee92e7729c
sesteban951 <sesteban@caltech.edu>
2026-03-16 16:14:39 -0700

    reworked joystick to use pygame instead since there is disconnect detection

--- deploy/commands.py
+++ deploy/commands.py
@@ -5,18 +5,14 @@
 ##
 
 # standard imports
-import argparse
-import subprocess
 from dataclasses import dataclass
 
 # other imports
-import time
-import yaml
+import pygame
 
 # ROS2 imports
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import Joy
 from std_msgs.msg import Float32MultiArray
 
 # directory imports
@@ -41,16 +37,15 @@ class JoystickState:
     RB: int = 0
     LMB: int = 0   # left middle button
     RMB: int = 0   # right middle button
-    XBOX: int = 0  # middle button with Xbox logo
     LS: int = 0    # left stick press
     RS: int = 0    # right stick press
     MB: int = 0    # middle button between LMB and RMB
 
     # Axes
-    LX: float = 0.0
-    LY: float = 0.0
-    RX: float = 0.0
-    RY: float = 0.0
+    LS_X: float = 0.0
+    LS_Y: float = 0.0
+    RS_X: float = 0.0
+    RS_Y: float = 0.0
     LT: float = 0.0
     RT: float = 0.0
     L_DPAD: float = 0.0  # D-PAD left
@@ -67,31 +62,10 @@ class CommandsNode(Node):
 
         super().__init__('command_node')
 
-        # load config file
-        self.config = self.load_config(config_path)
-
-        # launch joy_node as a subprocess
-        self.joy_process = subprocess.Popen(
-            ['ros2', 'run', 'joy', 'joy_node'],
-            stdout=subprocess.DEVNULL,
-            stderr=subprocess.DEVNULL
-        )
-
-        print("Launched joy_node as a subprocess.")
-
-        # joystick state
+        # initialize joystick
+        self.deadzone = 0.05
         self.joystick_state = JoystickState()
-
-        # ROS2 subscribers
-        self.joy_sub = self.create_subscription(Joy, 'joy', self.joy_callback, 10)
-        self.joy_msg = None
-        
-        # wait for the first joystick message to be received
-        t0 = time.time()
-        print("Waiting for the first joystick message...")
-        while self.joy_msg is None:
-            rclpy.spin_once(self, timeout_sec=0.01)
-        print("Received the first joystick message.")
+        self.init_joystick()
 
         # ROS2 publishers
         self.command_pub = self.create_publisher(Float32MultiArray, 'command', 10)
@@ -100,94 +74,131 @@ class CommandsNode(Node):
         command_freq = 100.0
         self.command_timer = self.create_timer(1.0 / command_freq, self.publish_command)
 
-        print("Command node initialized.")
+    # initialize the joystick
+    def init_joystick(self):
 
-    # load the config file
-    def load_config(self, config_path: str):
-        # open the config file and load it
-        config_path_full = ROOT_DIR + "/deploy/configs/" + config_path
-        with open(config_path_full, 'r') as f:
-            config = yaml.safe_load(f)
+        # initialize pygame and the joystick
+        pygame.init()
+        pygame.joystick.init()
 
-        print(f"Loaded config from [{config_path_full}].")
-
-        return config
-
-    # callback for joystick messages
-    def joy_callback(self, msg: Joy):
-        self.joy_msg = msg
-
-    # parse the joystick message
-    def joy_parse(self):
-        # buttons
-        self.joystick_state.A = self.joy_msg.buttons[0]
-        self.joystick_state.B = self.joy_msg.buttons[1]
-        self.joystick_state.X = self.joy_msg.buttons[2]
-        self.joystick_state.Y = self.joy_msg.buttons[3]
-        self.joystick_state.LB = self.joy_msg.buttons[4]
-        self.joystick_state.RB = self.joy_msg.buttons[5]
-        self.joystick_state.LMB = self.joy_msg.buttons[6]
-        self.joystick_state.RMB = self.joy_msg.buttons[7]
-        self.joystick_state.XBOX = self.joy_msg.buttons[8]
-        self.joystick_state.LS = self.joy_msg.buttons[9]
-        self.joystick_state.RS = self.joy_msg.buttons[10]
-        self.joystick_state.MB = self.joy_msg.buttons[11]
-
-        # joysticks
-        self.joystick_state.LX = self.joy_msg.axes[0]
-        self.joystick_state.LY = self.joy_msg.axes[1]
-        self.joystick_state.RX = self.joy_msg.axes[3]
-        self.joystick_state.RY = self.joy_msg.axes[4]
-
-        # triggers go from 1 (not pressed) to -1 (fully pressed), convert to 0 to 1
-        self.joystick_state.LT = -0.5 * self.joy_msg.axes[2] + 0.5
-        self.joystick_state.RT = -0.5 * self.joy_msg.axes[5] + 0.5
-
-        # D-PAD axes
-        DPAD_horizontal = self.joy_msg.axes[6]
-        if DPAD_horizontal == 1.0:
-            self.joystick_state.L_DPAD = 1.0
-            self.joystick_state.R_DPAD = 0.0
-        elif DPAD_horizontal == -1.0:
-            self.joystick_state.L_DPAD = 0.0
-            self.joystick_state.R_DPAD = 1.0
+        # check if a joystick is connected
+        self.joystick = None
+        if pygame.joystick.get_count() > 0:
+            self.joystick = pygame.joystick.Joystick(0)
+            self.joystick.init()
+            print(f"Joystick connected: [{self.joystick.get_name()}].")
         else:
-            self.joystick_state.L_DPAD = 0.0
-            self.joystick_state.R_DPAD = 0.0
+            print("No joystick found. Waiting for connection...")
 
-        DPAD_vertical = self.joy_msg.axes[7]
-        if DPAD_vertical == -1.0:
-            self.joystick_state.U_DPAD = 0.0
-            self.joystick_state.D_DPAD = 1.0
-        elif DPAD_vertical == 1.0:
-            self.joystick_state.U_DPAD = 1.0
-            self.joystick_state.D_DPAD = 0.0
-        else:
-            self.joystick_state.U_DPAD = 0.0
-            self.joystick_state.D_DPAD = 0.0
+    # update joystick state
+    def update_joystick_state(self):
 
-    # publish command based on the latest joystick message
+        # check for connect/disconnect events
+        for event in pygame.event.get():
+            if event.type == pygame.JOYDEVICEADDED and self.joystick is None:
+                self.joystick = pygame.joystick.Joystick(event.device_index)
+                self.joystick.init()
+                print(f"Joystick connected: [{self.joystick.get_name()}].")
+            elif event.type == pygame.JOYDEVICEREMOVED and self.joystick is not None:
+                print("Joystick disconnected.")
+                self.joystick = None
+                self.joystick_state = JoystickState()
+
+        # skip if no joystick connected
+        if self.joystick is None:
+            return
+
+        # update
+        try:
+            LS_X =  self.joystick.get_axis(0)
+            LS_Y = -self.joystick.get_axis(1) # invert y-axis
+            RS_X =  self.joystick.get_axis(3) 
+            RS_Y = -self.joystick.get_axis(4) # invert y-axis
+            self.joystick_state.LS_X = LS_X if abs(LS_X) > self.deadzone else 0.0
+            self.joystick_state.LS_Y = LS_Y if abs(LS_Y) > self.deadzone else 0.0
+            self.joystick_state.RS_X = RS_X if abs(RS_X) > self.deadzone else 0.0
+            self.joystick_state.RS_Y = RS_Y if abs(RS_Y) > self.deadzone else 0.0
+
+            # triggers
+            LT = 0.5 * self.joystick.get_axis(2) + 0.5
+            RT = 0.5 * self.joystick.get_axis(5) + 0.5
+            self.joystick_state.LT = LT if LT > self.deadzone else 0.0
+            self.joystick_state.RT = RT if RT > self.deadzone else 0.0
+
+            # D-PAD
+            DPAD = self.joystick.get_hat(0)
+            
+            DPAD_X = DPAD[0]
+            if DPAD_X <= -0.5:
+                self.joystick_state.L_DPAD = 1.0
+                self.joystick_state.R_DPAD = 0.0
+            elif DPAD_X >= 0.5:
+                self.joystick_state.L_DPAD = 0.0
+                self.joystick_state.R_DPAD = 1.0
+            else:
+                self.joystick_state.L_DPAD = 0.0
+                self.joystick_state.R_DPAD = 0.0
+
+            DPAD_Y = DPAD[1]
+            if DPAD_Y <= -0.5:
+                self.joystick_state.U_DPAD = 0.0
+                self.joystick_state.D_DPAD = 1.0
+            elif DPAD_Y >= 0.5:
+                self.joystick_state.U_DPAD = 1.0
+                self.joystick_state.D_DPAD = 0.0
+            else:
+                self.joystick_state.U_DPAD = 0.0
+                self.joystick_state.D_DPAD = 0.0
+
+            # buttons
+            self.joystick_state.A = self.joystick.get_button(0)
+            self.joystick_state.B = self.joystick.get_button(1)
+            self.joystick_state.X = self.joystick.get_button(2)
+            self.joystick_state.Y = self.joystick.get_button(3)
+            self.joystick_state.LB = self.joystick.get_button(4)
+            self.joystick_state.RB = self.joystick.get_button(5)
+            self.joystick_state.LMB = self.joystick.get_button(6)
+            self.joystick_state.RMB = self.joystick.get_button(7)
+            self.joystick_state.LS = self.joystick.get_button(9)
+            self.joystick_state.RS = self.joystick.get_button(10)
+            self.joystick_state.MB = self.joystick.get_button(11)
+
+        except pygame.error:
+            print("Joystick error.")
+            self.joystick = None
+            self.joystick_state = JoystickState()
+
+    # publish the command
     def publish_command(self):
-        # update the joystick
-        self.joy_parse()
+        # update the joystick state
+        self.update_joystick_state()
 
-        # convert the joystick state to a command message
-        vx_cmd = self.joystick_state.LY
-        vy_cmd = self.joystick_state.LX
-        omega_cmd = self.joystick_state.RX
+        # if no joystick connected, skip
+        if self.joystick is None:
+            # default to zero command
+            is_connected  = 0.0
+            vx_cmd = 0.0
+            vy_cmd = 0.0
+            omega_cmd = 0.0
+
+        # publish the command
+        else:
+            # convert the joystick state to a command message
+            is_connected = 1.0
+            vx_cmd = self.joystick_state.LS_Y
+            vy_cmd = self.joystick_state.LS_X
+            omega_cmd = -self.joystick_state.RS_X
 
         # publish the command
         cmd_msg = Float32MultiArray()
-        cmd_msg.data = [vx_cmd, vy_cmd, omega_cmd]
+        cmd_msg.data = [is_connected, vx_cmd, vy_cmd, omega_cmd]
 
         self.command_pub.publish(cmd_msg)
 
-    # terminate joy_node on shutdown
+    # graceful shutdown
     def destroy_node(self):
-        if self.joy_process is not None:
-            self.joy_process.terminate()
-            self.joy_process.wait()
-        super().destroy_node()
+        pygame.quit()           # shutdown pygame
+        super().destroy_node()  # destroy the ROS2 node
 
 
 ############################################################################
@@ -199,21 +210,8 @@ def main():
     # init ROS2
     rclpy.init()
 
-    # parse arguments
-    parser = argparse.ArgumentParser(
-        description='Asynchronous Simulation Node using Mujoco.'
-    )
-    # config path argument
-    parser.add_argument(
-        '--config',
-        type=str,
-        required=True,
-        help='Path to the Mujoco config yaml file. Example: "g1_29dof.yaml".'
-    )
-    args = parser.parse_args()
-
     # create command node
-    cmd_node = CommandsNode(args.config)
+    cmd_node = CommandsNode()
 
     # execute the simulation
     try:
